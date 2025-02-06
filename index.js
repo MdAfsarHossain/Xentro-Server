ce4080539a7f334166f34d4d0166f10004f1bdea
MdAfsarHossain <afsar291722@gmail.com>
2025-02-06 11:30:47 +0600

    Get all users data code added

--- index.js
+++ index.js
@@ -19,3 +19,34 @@ const corsOptions = {
 // Middleware
 app.use(cors(corsOptions));
 app.use(express.json());
+
+app.get("/users", async (req, res) => {
+  const search = req?.query?.search;
+  const city = req?.query?.city;
+  // console.log(search);
+
+  // let query = {};
+
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+
+    let result = data;
+
+    if (search) {
+      result = data?.filter((user) =>
+        user?.name.toLowerCase().includes(search.toLowerCase())
+      );
+    }
+
+    if (city && city !== "All City") {
+      result = result?.filter((user) =>
+        user?.address?.city.toLowerCase().includes(city.toLowerCase())
+      );
+    }
+
+    res.send(result);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
